35141aeed9e625933ccb165095cceb3f0b0c5cf7
TayyabQ <i200590@nu.edu.pk>
2025-08-11 15:51:49 +0500

    added missing placeholder in chat input

--- components/home/chat.tsx
+++ components/home/chat.tsx
@@ -8,7 +8,7 @@ const Chat = () => {
       <p className='text-[24px] md:text-[28px] lg:text-[40px] xl:text-[48px] 2xl:text-[72px] text-header-purple font-bold my-2'>Welcome to AI Assisstant</p>
       <p className='text-[11px] md:text-[18px] 2xl:text-[28px]'>I am your assisstant. You can ask me anything you want to ask!</p>
       <p>Hello</p>
-      <Textarea className='min-h-28 2xl:min-h-40 mt-8 border-header-purple shadow-sm'/>
+      <Textarea className='min-h-28 2xl:min-h-40 mt-8 border-header-purple shadow-sm' placeholder='Enter prompt ...'/>
       <Button className='w-50 mt-2 2xl:text-[24px] bg-header-purple text-white' variant='default' size='default'>Go</Button>
     </div>
   )
