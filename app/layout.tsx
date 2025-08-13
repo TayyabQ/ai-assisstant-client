873927af6b14567b8e105405bae5d4cbece4c84b
TayyabQ <i200590@nu.edu.pk>
2025-08-13 19:52:59 +0500

    authentication with NextAuth using github

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import SessionWrapper from "./SessionWrapper/page";
 
 export const metadata: Metadata = {
   title: "AI Assisstant",
@@ -13,9 +14,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <SessionWrapper>
       <body>
         {children}
       </body>
+      </SessionWrapper>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,34 @@
-import Dashboard from "./dashboard/page";
+// import Dashboard from "./dashboard/page";
 // import Login from "./login/page";
 // import Signup from "./signup/page";
 
-export default function Home() {
-  return (
-    <Dashboard/>
-    // <Login/>
-    // <Signup/>
-  );
+// export default function Home() {
+//   return (
+//     <Dashboard/>
+//     // <Login/>
+//     // <Signup/>
+//   );
+// }
+
+'use client';
+
+import { useSession, signIn, signOut } from "next-auth/react"
+import Dashboard from "./dashboard/page";
+
+export default function Component() {
+  const { data: session } = useSession()
+  console.log(session)
+  if(session) {
+    return <>
+      <Dashboard/>
+      <br/>
+      <br/>
+      <br/>
+      <button onClick={() => signOut()}>Sign out</button>
+    </>
+  }
+  return <>
+    Not signed in <br/>
+    <button onClick={() => signIn()}>Sign in</button>
+  </>
 }
