b71f9fdc45d11d69e81180e289756a71de7f2e08
TayyabQ <i200590@nu.edu.pk>
2025-08-15 12:46:20 +0500

    PR update with reviewed change

--- components/home/chat.tsx
+++ components/home/chat.tsx
@@ -7,7 +7,6 @@ const Chat = () => {
     <div className='min-h-screen flex flex-col items-center justify-center px-10 md:px-20 lg:px-40'>
       <p className='text-[24px] md:text-[28px] lg:text-[40px] xl:text-[48px] 2xl:text-[72px] text-header-purple font-bold my-2'>Welcome to AI Assisstant</p>
       <p className='text-[11px] md:text-[18px] 2xl:text-[28px]'>I am your assisstant. You can ask me anything you want to ask!</p>
-      <p>Hello</p>
       <Textarea className='min-h-28 2xl:min-h-40 mt-8 border-header-purple shadow-sm' placeholder='Enter prompt ...'/>
       <Button className='w-50 mt-2 2xl:text-[24px] bg-header-purple text-white' variant='default' size='default'>Go</Button>
     </div>
