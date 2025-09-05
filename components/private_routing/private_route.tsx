b0d30490dce8fbe740f9e117063cee4dc4741f2c
TayyabQ <i200590@nu.edu.pk>
2025-09-05 17:54:44 +0500

    Save user in db

--- components/private_routing/private_route.tsx
+++ components/private_routing/private_route.tsx
@@ -2,6 +2,9 @@
 
 import Login from "@/app/login/page";
 import { useSession } from "next-auth/react";
+import { useEffect } from "react";
+
+import useFetch from "@/components/hooks/useFetch";
 
 export default function PrivateRoute({
   children,
@@ -9,6 +12,39 @@ export default function PrivateRoute({
   children: React.ReactNode;
 }) {
   const { data: session } = useSession();
+  console.log(session);
+  const expiry = session?.expires;
+  const user = session?.user;
+
+  const { fetchdata } = useFetch();
+
+  // Call NestJS API to save user data when session exists
+  useEffect(() => {
+    if (session && user) {
+      const saveUser = async () => {
+        fetchdata({
+          url: "http://localhost:3001/user/save",
+          method: "POST",
+          body: {
+            email: user.email,
+            name: user.name,
+            image: user.image,
+            expiry: expiry,
+          },
+          onSuccess: async (response) => {
+            const data = await response.json();
+            console.log(data.message);
+          },
+          onFailure: () => {
+            console.error("Failed to save user");
+          },
+        });
+      };
+
+      saveUser();
+    }
+  }, [session, user]);
+
   if (session) {
     return children;
   }
